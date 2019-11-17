b601f6c5cb87be27cc300b01fc5751c22a97a0a2
Sean Aguinaga <aguinaga.sean@gmail.com>
2019-11-17 14:02:22 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -327,6 +327,7 @@ export default class App extends Component {
             <SettingsIcon onClick={this.openModal} className="mobile-toggle" />
           </h2>
           <form
+            method="post"
             netlify
             name="new-todo"
             className="todo-create-wrapper"
