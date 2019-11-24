92522324dab81ad3380db5b971a778a27f7d0270
Sean Aguinaga <aguinaga.sean@gmail.com>
2019-11-24 11:50:28 -0800

    Update todos-update.js

--- functions/todos-update.js
+++ functions/todos-update.js
@@ -13,24 +13,39 @@ const client = new faunadb.Client({
 });
 
 exports.handler = (event, context) => {
-  const data = JSON.parse(event.body);
-  const id = getId(event.path);
-  console.log(`Function 'todo-update' invoked. update id: ${id}`);
-  return client
-    .query(q.Update(q.Ref(`classes/Todos/${id}`), { data }))
-    .then(response => {
-      console.log("success", response);
-      return {
-        headers,
-        statusCode: 200,
-        body: JSON.stringify(response)
-      };
-    })
-    .catch(error => {
-      console.log("error", error);
-      return {
-        statusCode: 400,
-        body: JSON.stringify(error)
-      };
-    });
+  if (event.httpMethod !== "POST" || !event.body) {
+    return {
+      headers,
+      statusCode: 200,
+      body: "NOop"
+    };
+  }
+  try {
+    const data = JSON.parse(event.body);
+    const id = getId(event.path);
+    console.log(`Function 'todo-update' invoked. update id: ${id}`);
+    return client
+      .query(q.Update(q.Ref(`classes/Todos/${id}`), { data }))
+      .then(response => {
+        console.log("success", response);
+        return {
+          headers,
+          statusCode: 200,
+          body: JSON.stringify(response)
+        };
+      })
+      .catch(error => {
+        console.log("error", error);
+        return {
+          statusCode: 400,
+          body: JSON.stringify(error)
+        };
+      });
+  } catch (error) {
+    return {
+      headers,
+      statusCode: 500,
+      body: "err"
+    };
+  }
 };
