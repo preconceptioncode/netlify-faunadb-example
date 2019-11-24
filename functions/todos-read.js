9ed7292dd44794c803f632cd3c6795da06ed8be6
Sean Aguinaga <aguinaga.sean@gmail.com>
2019-11-24 10:29:44 -0800

    Update todos-read.js

--- functions/todos-read.js
+++ functions/todos-read.js
@@ -1,27 +1,36 @@
 /* Import faunaDB sdk */
-const faunadb = require('faunadb')
-const getId = require('./utils/getId')
+const faunadb = require("faunadb");
+const getId = require("./utils/getId");
 
-const q = faunadb.query
+const headers = {
+  "Access-Control-Allow-Origin": "Content-Type",
+  "Access-Control-Allow-Headers": "*"
+};
+
+const q = faunadb.query;
 const client = new faunadb.Client({
   secret: process.env.FAUNADB_SERVER_SECRET
-})
+});
 
 exports.handler = (event, context) => {
-  const id = getId(event.path)
-  console.log(`Function 'todo-read' invoked. Read id: ${id}`)
-  return client.query(q.Get(q.Ref(`classes/todos/${id}`)))
-    .then((response) => {
-      console.log('success', response)
+  const id = getId(event.path);
+  console.log(`Function 'todo-read' invoked. Read id: ${id}`);
+  return client
+    .query(q.Get(q.Ref(`classes/todos/${id}`)))
+    .then(response => {
+      console.log("success", response);
       return {
+        headers,
         statusCode: 200,
         body: JSON.stringify(response)
-      }
-    }).catch((error) => {
-      console.log('error', error)
+      };
+    })
+    .catch(error => {
+      console.log("error", error);
       return {
+        headers,
         statusCode: 400,
         body: JSON.stringify(error)
-      }
-    })
-}
+      };
+    });
+};
