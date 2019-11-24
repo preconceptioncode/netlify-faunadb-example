4ab5cbd20317465359ed55871d1464c67b8bc20f
Sean Aguinaga <aguinaga.sean@gmail.com>
2019-11-24 09:36:08 -0800

    Added Different Headers

--- functions/log.js
+++ functions/log.js
@@ -1,3 +1,31 @@
-export async function handler(event) {
-  console.log(event);
-}
+const axios = require("axios");
+
+exports.handler = async () => {
+  try {
+    const response = await axios.post(
+      "https://tntbhn1sdhd.SANDBOX.verygoodproxy.com/.netlify/functions/todos-create",
+      { title: "Sean", completed: false }
+    );
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: response.data,
+        status: "succeeded",
+        connected: true,
+        authenticated: true
+      })
+    };
+  } catch (error) {
+    console.log("LogIn Failed: Transaction ID", error.transaction_id);
+    console.log("ERROR", error); // output to netlify function log
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        error,
+        connected: true,
+        authenticated: false,
+        status: "failed"
+      }) // Could be a custom message or object i.e. JSON.stringify(err)
+    };
+  }
+};
--- functions/todos-create.js
+++ functions/todos-create.js
@@ -1,6 +1,11 @@
 /* Import faunaDB sdk */
 const faunadb = require("faunadb");
 
+const headers = {
+  "Access-Control-Allow-Origin": "Content-Type",
+  "Access-Control-Allow-Headers": "*"
+};
+
 /* configure faunaDB Client with our secret */
 const q = faunadb.query;
 const client = new faunadb.Client({
@@ -9,35 +14,46 @@ const client = new faunadb.Client({
 
 /* export our lambda function as named "handler" export */
 exports.handler = async (event, context) => {
+  if (event.httpMethod !== "POST" || !event.body) {
+    return {
+      headers,
+      statusCode: 200,
+      body: "NOop"
+    };
+  }
+
+  try {
+    const data = JSON.parse(event.body);
+    console.log("Function `todo-create` invoked", data);
+    const todoItem = {
+      data: data
+    };
+    /* construct the fauna query */
+    return client
+      .query(q.Create(q.Ref("classes/todos"), todoItem))
+      .then(response => {
+        console.log("success", response);
+        /* Success! return the response with statusCode 200 */
+        return {
+          statusCode: 200,
+          headers,
+          body: JSON.stringify(response)
+        };
+      })
+      .catch(error => {
+        console.log("error", error);
+        /* Error! return the error with statusCode 400 */
+        return {
+          statusCode: 400,
+          body: JSON.stringify(error)
+        };
+      });
+  } catch (error) {
+    return {
+      headers,
+      statusCode: 500,
+      body: "err"
+    };
+  }
   /* parse the string body into a useable JS object */
-  const data = JSON.parse(event.body);
-  console.log("Function `todo-create` invoked", data);
-  const todoItem = {
-    data: data
-  };
-  /* construct the fauna query */
-  return client
-    .query(q.Create(q.Ref("classes/todos"), todoItem))
-    .then(response => {
-      console.log("success", response);
-      /* Success! return the response with statusCode 200 */
-      return {
-        statusCode: 200,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods:":
-            "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token"
-        },
-        body: JSON.stringify(response)
-      };
-    })
-    .catch(error => {
-      console.log("error", error);
-      /* Error! return the error with statusCode 400 */
-      return {
-        statusCode: 400,
-        body: JSON.stringify(error)
-      };
-    });
 };
